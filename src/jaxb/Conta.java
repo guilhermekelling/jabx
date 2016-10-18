846e67342c97783b730d1bef29827647e6ca8b8b
guilhermekelling <Guilherme@DTI02.cpa.local>
2016-10-18 10:01:46 -0200

    Iniciando o projeto

---
+++ src/jaxb/Conta.java
@@ -0,0 +1,23 @@
+package jaxb;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Conta {
+	private double saldo;
+	private double limite;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}	
+	public double getLimite() {
+		return limite;
+	}
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+
+}
